28211bdc229ea807a15c69b03976207620607169
box-7 <hayainada1981@yahoo.co.jp>
2024-12-08 11:33:50 +0900

    prettierでnpm run formatを実行

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-        return (
-                <>
-                  <h1>Hello World</h1>
-                </>
-              );
-
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
 }
 
-export default App
-
+export default App;
 
 // const [count, setCount] = useState(0)
 // return (
