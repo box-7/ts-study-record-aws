4baa4159069d839d8e110025cf4225aa3691a71e
box-7 <hayainada1981@yahoo.co.jp>
2024-12-14 17:14:52 +0900

    jest.setup.tsをqiita記事を見て修正

--- jest.setup.ts
+++ jest.setup.ts
@@ -6,6 +6,13 @@ global.structuredClone = (val) => {
         return JSON.parse(JSON.stringify(val));
       };
 
+      if(!global.structuredClone){
+        global.structuredClone = function structuredClone(objectToClone: any) {
+              const stringified = JSON.stringify(objectToClone);
+              const parsed = JSON.parse(stringified);
+              return parsed;
+            }
+    }
 
 // structuredClone のポリフィル
 // if (typeof global.structuredClone === 'undefined') {
