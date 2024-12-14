9d975f4a6a8682ee3da0480c01b7e1585881e443
box-7 <hayainada1981@yahoo.co.jp>
2024-12-14 15:36:40 +0900

    jest.setup.ts修正

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,12 +1,22 @@
 import '@testing-library/jest-dom';
-import 'core-js/actual/structured-clone';
+import 'dotenv/config';
 
-require('dotenv').config();
-
-if (!global.structuredClone) {
-  global.structuredClone = function structuredClone(objectToClone: any) {
-    const stringified = JSON.stringify(objectToClone);
-    const parsed = JSON.parse(stringified);
-    return parsed;
+// structuredClone のポリフィル
+if (typeof global.structuredClone === 'undefined') {
+  global.structuredClone = (value: any) => {
+    return JSON.parse(JSON.stringify(value));
   };
 }
+
+// import '@testing-library/jest-dom';
+// import 'core-js/actual/structured-clone';
+
+// require('dotenv').config();
+
+// if (!global.structuredClone) {
+//   global.structuredClone = function structuredClone(objectToClone: any) {
+//     const stringified = JSON.stringify(objectToClone);
+//     const parsed = JSON.parse(stringified);
+//     return parsed;
+//   };
+// }
