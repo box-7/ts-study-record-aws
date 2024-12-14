fcbb1cec2e504db1e892f57a75e444efa9919447
box-7 <hayainada1981@yahoo.co.jp>
2024-12-14 15:13:16 +0900

    AppComponent.spec.tsxを修正

--- src/tests/AppComponent.spec.tsx
+++ src/tests/AppComponent.spec.tsx
@@ -1,15 +1,34 @@
-import App from '../App';
-import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import { render, screen } from '@testing-library/react';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import App from '../App';
 
 describe('title', () => {
   it('should render title', () => {
-      render(
-        <ChakraProvider>
-      <App />
-      </ChakraProvider>;
-      );
+    render(
+      <ChakraProvider value={defaultSystem}>
+        <App />
+      </ChakraProvider>
+    );
     expect(screen.getByText('学習記録一覧!')).toBeInTheDocument();
   });
 });
+
+// import App from '../App';
+// import { render, screen } from '@testing-library/react';
+// import React from 'react';
+// import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+// // 必要に応じてテーマをインポート
+// const theme = extendTheme({});
+
+// describe('title', () => {
+//   it('should render title', () => {
+//       render(
+//         <ChakraProvider theme={theme}>
+//       <App />
+//       </ChakraProvider>;
+//       );
+//     expect(screen.getByText('学習記録一覧!')).toBeInTheDocument();
+//   });
+// });
