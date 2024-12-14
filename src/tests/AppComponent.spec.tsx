ba4a6ce77b01c5c0cdb014033220859f94f65f4a
box-7 <hayainada1981@yahoo.co.jp>
2024-12-14 15:07:07 +0900

    AppComponent.spec.tsxを修正

--- src/tests/AppComponent.spec.tsx
+++ src/tests/AppComponent.spec.tsx
@@ -1,9 +1,15 @@
 import App from '../App';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 describe('title', () => {
   it('should render title', () => {
-    render(<App />);
+      render(
+        <ChakraProvider>
+      <App />
+      </ChakraProvider>;
+      );
     expect(screen.getByText('学習記録一覧!')).toBeInTheDocument();
   });
 });
