670eeb4d2cb829fef65acd2c4ebee908541aa485
box-7 <hayainada1981@yahoo.co.jp>
2025-01-02 15:58:26 +0900

    ymlファイルのand cacheをコメントアウト

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -13,3 +13,19 @@ export default supabase;
 
 // const supabaseUrl = process.env.VITE_SUPABASE_URL;
 // const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
+
+
+
+// // createClient関数をインポート この関数を使ってSupabaseクライアントを作成
+// import { createClient } from "@supabase/supabase-js";
+
+// // supabaseのHPのAPISettingsから、ProjectURLとAPI Keysを取得
+// // vite.config.jsの設定を変えて、process.env.VITE_SUPABASE_URL;で環境変数を取得
+// const supabaseUrl = process.env.VITE_SUPABASE_URL;
+// const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
+
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export default supabase;
+
+
