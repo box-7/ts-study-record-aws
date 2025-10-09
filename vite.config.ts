a7beb40f5ade4a5e0b56df9ac4b09d5cb2484d19
box-7 <hayainada1981@yahoo.co.jp>
2025-10-09 10:38:02 +0900

    Add: srcをfrontendディレクトリに移動するためvite config、index.html変更

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,15 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import envCompatible from 'vite-plugin-env-compatible';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    envCompatible({
-      prefix: 'VITE', // 環境変数のプレフィックス
-      mountedPath: 'process.env', // 環境変数をマウントするパス
-    }),
-  ],
+        root: 'frontend',
+        plugins: [
+                react(),
+                tsconfigPaths(),
+                envCompatible({
+                        prefix: 'VITE', // 環境変数のプレフィックス
+                        mountedPath: 'process.env', // 環境変数をマウントするパス
+                }),
+        ],
 });
 
 // 設定の詳細
@@ -25,31 +26,31 @@ export default defineConfig({
 // plugins
 // plugins 配列には、Vite のプラグインを設定
 // この例では、3つのプラグインが設定されている
-        // a. react()
-        // react プラグインは、Vite で React をサポートするためのプラグイン
-        // React コンポーネントを使用した開発が可能になる
+// a. react()
+// react プラグインは、Vite で React をサポートするためのプラグイン
+// React コンポーネントを使用した開発が可能になる
 
-        // b. tsconfigPaths()
-        // tsconfigPaths プラグインは、TypeScript の tsconfig.json ファイルに定義されたパスエイリアスを Vite で解決するためのプラグイン
-        // tsconfig.json に設定されたパスエイリアスを Vite のビルドプロセスで使用できます。
+// b. tsconfigPaths()
+// tsconfigPaths プラグインは、TypeScript の tsconfig.json ファイルに定義されたパスエイリアスを Vite で解決するためのプラグイン
+// tsconfig.json に設定されたパスエイリアスを Vite のビルドプロセスで使用できます。
 
-        // c. envCompatible
-        // envCompatible プラグインは、環境変数を Vite のプロジェクトに適用するためのプラグイン
-        // 特定のプレフィックスを持つ環境変数を読み込み、指定されたパスにマウント
+// c. envCompatible
+// envCompatible プラグインは、環境変数を Vite のプロジェクトに適用するためのプラグイン
+// 特定のプレフィックスを持つ環境変数を読み込み、指定されたパスにマウント
 
 // prefix: 'VITE':
 // 環境変数のプレフィックスを指定
-        // プレフィックス（prefix）とは、文字列の先頭に付加される特定の文字列や記号のことを指す
+// プレフィックス（prefix）とは、文字列の先頭に付加される特定の文字列や記号のことを指す
 // VITE で始まる環境変数が対象となる
 
 // mountedPath: 'process.env':
 // 環境変数をマウントするパスを指定
 // 環境変数が process.env にマウント
-        // 1 .env ファイル
-        // VITE_API_URL=https://api.example.com
-        // 2 vite.config.ts
-        // vite.config.ts ファイルで envCompatible プラグインを使用して、環境変数を process.env にマウント
-        // 3 アプリケーション内で process.env を通じて環境変数にアクセス
-        // console.log(process.env.VITE_API_URL); // 環境変数を使用
+// 1 .env ファイル
+// VITE_API_URL=https://api.example.com
+// 2 vite.config.ts
+// vite.config.ts ファイルで envCompatible プラグインを使用して、環境変数を process.env にマウント
+// 3 アプリケーション内で process.env を通じて環境変数にアクセス
+// console.log(process.env.VITE_API_URL); // 環境変数を使用
 
 
