2daffffce448e86a6557208d0e04e8d799fc51eb
box-7 <hayainada1981@yahoo.co.jp>
2025-10-24 16:30:55 +0900

    chore: db.ts不要なコマンド削除

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,10 +1,5 @@
 console.log('--- db.ts START ---');
 // 生成コマンド
-// npx prisma generate
-// import { PrismaClient } from '@prisma/client';
-// const prisma = new PrismaClient();
-// export default prisma;
-
 import { PrismaClient } from '../generated/prisma/index.js';
 
 const prisma = new PrismaClient();
