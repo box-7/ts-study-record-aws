8a62e81de2c095e77d32956735e7cb7be98541e3
box-7 <hayainada1981@yahoo.co.jp>
2025-10-25 14:34:30 +0900

    Add: ローカルでマイグレーションし、AWSで変更できているか確認

---
+++ backend/prisma/postgres/migrations/20251025052733_test_model10251420/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "TestModel7" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "TestModel7_pkey" PRIMARY KEY ("id")
+);
